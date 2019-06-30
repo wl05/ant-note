84f8a6d946e8de19d029f7f732811673932e7d08
wangle <2929712050@qq.com>
2019-06-30 19:53:22 +0800

    初始化

---
+++ miniprogram/pages/me/me.js
@@ -0,0 +1,33 @@
+Page({
+  data: {
+    hello: 'world',
+    avatarUrl: '',
+    userInfo: {}
+  },
+  onLoad: function () {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log(res.userInfo)
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+})
